2b44fe987d5934e06d39f22466b13aa5e137b01f
soleilvermeil <julien.leuenberger@outlook.com>
2025-02-22 13:01:21 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+from src.interface import Player, Character
+from src.genshintypes import Stat
+
+
+def main() -> None:
+
+    # Create the player object
+    player: Player = Player(uid=703047530)
+
+    # Get a character and print their stats
+    character: Character = player.characters[0]
+    print(character.name)
+    for s in Stat:
+        print(f"{s}: {character.stats.get(s):.1f}")
+
+
+if __name__ == "__main__":
+    main()
