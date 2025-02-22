48b0d34dadc535ad33141c93ed1886ea7d0aa1c7
soleilvermeil <julien.leuenberger@outlook.com>
2025-02-22 13:02:25 +0100

    Changed default language to english

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,7 @@
 import os
 
 
-LANG: str = "fr"
+LANG: str = "en"
 BASE_URL: str = "https://enka.network/api/uid"
 CACHE_FOLDER: str = os.path.join("cache")
 CONSTANTS_FOLDER: str = os.path.join("src", "constants")
